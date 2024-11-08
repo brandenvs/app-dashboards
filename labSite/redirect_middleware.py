9fd4b8632d4612306bac5e5a786a22e5e53e6d42
brandenvs <92578986+brandenvs@users.noreply.github.com>
2024-11-08 13:44:33 +0200

    Fixed custom middleware

--- labSite/redirect_middleware.py
+++ labSite/redirect_middleware.py
@@ -24,33 +24,3 @@ class DomainRedirectMiddleware:
         # Default response
         response = self.get_response(request)
         return response
-
-
-
-
-class DomainRedirectMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        # Get the host and check if the user was already redirected
-        current_domain = request.get_host()
-        already_redirected = request.session.get('already_redirected', False)
-
-        # Define target paths based on the domain
-        if current_domain == 'stadprin.com' and not already_redirected:
-            request.session['already_redirected'] = True
-            return HttpResponseRedirect(request, 'partials/standalone_home.html')
-
-
-        elif current_domain == 'spt.bcodelabs.com' and not already_redirected:
-            request.session['already_redirected'] = True
-            return redirect('/portal')
-
-        # Clear the redirect flag for future requests
-        if already_redirected:
-            request.session['already_redirected'] = False
-
-        # Default response
-        response = self.get_response(request)
-        return response
